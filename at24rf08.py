b008cf581475d64080473dff54d53016a1ea3d75
Michael Büchler <michael.buechler@posteo.net>
2023-09-21 00:56:43 +0200

    Add comments

--- at24rf08.py
+++ at24rf08.py
@@ -20,8 +20,6 @@ prot_n = Pin(6,  Pin.OUT, value=1)
 wp     = Pin(28, Pin.OUT, value=1)
 i2c    = I2C(1, freq=40000, scl=Pin(27), sda=Pin(26))
 
-# APP page at memory addresses 0x00 to 0x0f
-# ID  page at memory addresses 0x10 to 0x1f
 def dump():
     with open('dump', 'wb') as f:
         for block in range(BLOCKS):
@@ -32,11 +30,17 @@ def dump():
                 print(f"The device address is {hex(devaddr)}")
                 f.write(i2c.readfrom_mem(devaddr, addr, PAGESIZE))
 
+# just dump the APP bytes for the protection of the main 8 blocks of memory
+# APP page at memory addresses 0x00 to 0x0f
+# ID  page at memory addresses 0x10 to 0x1f
 def dump_app():
     for i in range(8):
         val = i2c.readfrom_mem(devaddr_extra, i, 1)
         print(f"APP byte {i}: {val}")
 
+# a: first block to unlock
+# b: last block to unlock + 1 
+# example: unlock(1, 2) to only unlock block 1
 def unlock(a, b):
     wp.value(0)
     for i in range(a,b):
